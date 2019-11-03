a2d8e8cca0ae6da2f7b2cf723c2aba303eab73cf
CindyPotvin <blog@cindypotvin.com>
2019-11-03 11:45:31 -0500

    Added socket.io to push latest weather

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require('express');  /*Web framework for Node*/
 const bodyParser = require('body-parser'); /* Parses incoming requests (for json)*/
 const morgan  = require('morgan') /* Show access logs in console */
+var http = require('http');
+const socketIo = require('socket.io'); /* To send data to the client from the server using websockets*/
 const app = express();
 
 const config = require('./config/config.js');
 const weather = require('./weather.js');
+
+const server = http.createServer(app);
+const io = socketIo(server);
 
 app.use(morgan('combined'));
 app.use(express.static(__dirname + '/static'));
@@ -14,10 +19,16 @@ app.get('/api/hello', (request, response) => {
     response.send({ express: 'Hello From Express' });
   });
   
+// Adds a message to the console when a client connects.
+io.on('connection', function(socket){
+    console.log('a user connected');
+  });
+
 // Adds the latest temperature and humidity to file
 app.post('/api/weather', (request, response) => {
     const weatherData = request.body.weather;
     weather.addNewWeather(weatherData, config, app.settings.env);
+    io.emit("WeatherAPI", weatherData, { for: 'everyone' });
     response.end();
     });
 
--- client/src/Weather/Weather.js
+++ client/src/Weather/Weather.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import socketIOClient from "socket.io-client";
 import axios from "axios";
 
 let Weather = () => {
@@ -8,17 +9,22 @@ let Weather = () => {
     currentPressure: ""
   });
 
+  const endpoint = 'http://localhost:3000';
+
   useEffect( () => {
     const fetchData = async () => {
-      const result = await axios.get('http://localhost:3000/api/weather');
+      const result = await axios.get(endpoint + '/api/weather');
       setWeather({
         currentTemperature: result.data.temperature,
         currentHumidity: result.data.humidity,
         currentPressure: result.data.pressure
       });
     };
-
+    // Fetch latest data from the server
     fetchData();
+    // Setup the socket.io socket to listen to new weather
+    const socket = socketIOClient(endpoint);
+    socket.on("WeatherAPI", data => console.log(data));
   }, []);
 
   return (
